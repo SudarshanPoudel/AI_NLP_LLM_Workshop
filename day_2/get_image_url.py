b51c0ec8b6dcd014b020c2274b4cd47bf1826fdf
Sudarshan Poudel <poudelsudarshan650@gmail.com>
2025-06-29 22:13:07 +0545

    quick fix

--- day_2/get_image_url.py
+++ day_2/get_image_url.py
@@ -1,12 +1,12 @@
 import requests
 
-TMDB_API_KEY = "aed0f7f5a89450f11167d5e0254cff70"
+TMDB_KEY = "aed0f7f5a89450f11167d5e0254cff70"
 BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
 
-def get_poster_url(movie_id: int) -> str:
+def get_image_url(movie_id: int) -> str:
     url = f"https://api.themoviedb.org/3/movie/{movie_id}"
     params = {
-        "api_key": TMDB_API_KEY,
+        "api_key": TMDB_KEY,
         "language": "en-US"
     }
     response = requests.get(url, params=params)
